dea6008ff41e166befe12bf6885468a141038c12
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-03-18 18:02:51 -0400

    Update AmountCards.tsx

--- components/dashboard/AmountCards.tsx
+++ components/dashboard/AmountCards.tsx
@@ -12,15 +12,15 @@ export default function Component ({ tokens, mul, bigmul }) {
                             if (!JSON.parse(LS_tokensArrayObj)[aToken]) return 
                             let aSelectedTimeframe = JSON.parse(LS_tokensArrayObj)[aToken]
                             let aCrystal = 0
-                            let localMul = index == 0 ? mul*2 : mul
-                            let localBigMul = index == 0 ? bigmul*2 : bigmul
+                            let localMul = index < 2 ? mul*2 : mul
+                            let localBigMul = index < 2 ? bigmul*2 : bigmul
                             // let totalValue = aSelectedTimeframe.reduce((prev, curr, i, acc)=>{
 
                             // },0)
                             // if (!index) return // hide  btc
                             return (
                                 <div className='box-shadow-1   flex-1 ord-r-8' key={index}>
-                                    <div className='flex   flex-justify-start px-6' style={index==0?{boxShadow:"0 0 1px green"}:{}}>
+                                    <div className='flex   flex-justify-start px-6' style={index<2?{boxShadow:"0 0 1px green"}:{}}>
                                         <div className=' py-2 '>
                                             <div className='ims-tx-faded py-1 flex gap-1'>
                                                 {aToken.toUpperCase()} {index==0 && <div>(X*2)</div>}
