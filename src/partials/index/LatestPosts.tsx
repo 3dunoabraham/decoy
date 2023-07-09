d10f98b58ea7539d061d658a8222c5624238d0dc
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-07-08 20:47:20 -0400

    projects and inter

---
+++ src/partials/index/LatestPosts.tsx
@@ -0,0 +1,10 @@
+
+export function LatestPosts ({}) {
+  return (
+    <div className="bg-w-10  bg-glass-10 w-max-600px w-100 pa-8 bord-r-50 py-100">
+      LatestPosts
+    </div>
+  )
+}
+
+export default LatestPosts
